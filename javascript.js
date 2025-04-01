27ee25831b7afda59c03dbac8dd3e2717341f2f3
Exocet-1 <205508512+Exocet-1@users.noreply.github.com>
2025-04-01 15:38:29 -0300

    Logic for computer moves added.
    Added getComputerChoice for variable opponent play based on a difficulty setting and randomness; Added image assets; Added skeleton CSS file.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-alert('Hello!');
+function getComputerChoice(difficultyLevel = 0.2, playerInput = 0) {
+    let diceRoll = Math.random();
+    let output = playerInput;
+    //Win
+    if (diceRoll > difficultyLevel && Math.random() > difficultyLevel) {
+        return playerInput == 0 ? 2 : playerInput - 1;
+    }
+    //Draw
+    else if (diceRoll > difficultyLevel){
+        return playerInput;
+    }
+    //Lose
+    else{
+        return playerInput == 2 ? 0 : playerInput + 1;
+    }
+}
